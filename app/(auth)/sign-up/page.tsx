cea4912cee3dd624c2e27cdd0041c5b7f20e0ff9
franckrm <franckrm2001@gmail.com>
2026-03-21 07:57:55 -0300

    feat: initial commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,6 @@
+import AuthForm from "@/app/components/AuthForm";
+import React from "react";
+
+const SingUp = () => <AuthForm type="sign-up" />;
+
+export default SingUp;
---
+++ app/components/AuthForm.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+type TypeForm = "sign-in" | "sign-up";
+
+const AuthForm = ({ type }: { type: TypeForm }) => {
+  return <div></div>;
+};
+
+export default AuthForm;
