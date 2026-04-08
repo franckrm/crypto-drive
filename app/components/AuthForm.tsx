c460b6264d27733ebd13a9c8e75819e97608b100
franckrm <franckrm2001@gmail.com>
2026-04-08 16:59:03 -0300

    feat: add link option to sign-in and sign-up in authform

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -22,6 +22,7 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 const formSchema = z.object({
   fullName: z
@@ -58,72 +59,90 @@ const AuthForm = ({ type }: { type: TypeForm }) => {
                 control={form.control}
                 render={({ field, fieldState }) => (
                   <>
-                  <Field
-                    data-invalid={fieldState.invalid}
-                    className="shad-form-item"
-                  >
-                    <FieldLabel
-                      htmlFor="form-rhf-input-fullName"
-                      className="shad-form-label"
+                    <Field
+                      data-invalid={fieldState.invalid}
+                      className="shad-form-item"
                     >
-                      Full Name
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-input-fullName"
-                      // aria-invalid={fieldState.invalid}
-                      placeholder="Enter your full name"
-                      autoComplete="vz"
-                      className="shad-input"
-                    />
-                   
-                  </Field>
-                   {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]}  className="shad-form-message"/>
-                    )}
-                  </>
-                  
-                )}
-              />
-              <Controller
-                name="email"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <>
-                  <Field
-                    data-invalid={fieldState.invalid}
-                    className="shad-form-item"
-                  >
-                    <FieldLabel
-                      htmlFor="form-rhf-input-email"
-                      className="shad-form-label"
-                    >
-                      Email
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-input-email"
-                      aria-invalid={false}
-                      placeholder="Enter your email"
-                      autoComplete="email"
-                      className="shad-input"
-                    />
-                   
-                  </Field>
-                      {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]}  className="shad-form-message"/>
+                      <FieldLabel
+                        htmlFor="form-rhf-input-fullName"
+                        className="shad-form-label"
+                      >
+                        Full Name
+                      </FieldLabel>
+                      <Input
+                        {...field}
+                        id="form-rhf-input-fullName"
+                        // aria-invalid={fieldState.invalid}
+                        placeholder="Enter your full name"
+                        autoComplete="vz"
+                        className="shad-input"
+                      />
+                    </Field>
+                    {fieldState.invalid && (
+                      <FieldError
+                        errors={[fieldState.error]}
+                        className="shad-form-message"
+                      />
                     )}
                   </>
                 )}
               />
             </>
           )}
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <>
+                <Field
+                  data-invalid={fieldState.invalid}
+                  className="shad-form-item"
+                >
+                  <FieldLabel
+                    htmlFor="form-rhf-input-email"
+                    className="shad-form-label"
+                  >
+                    Email
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-input-email"
+                    aria-invalid={false}
+                    placeholder="Enter your email"
+                    autoComplete="email"
+                    className="shad-input"
+                  />
+                </Field>
+                <Field>
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="shad-form-message"
+                    />
+                  )}
+                </Field>
+              </>
+            )}
+          />
         </FieldGroup>
         <Field orientation="horizontal">
           <Button type="submit" className="form-submit-button">
             {type == "sign-in" ? "Sign In" : "Sign Up"}
           </Button>
         </Field>
+        <div className="body-2 flex justify-center">
+          <p className="text-light-100">
+            {type == "sign-in"
+              ? "Don't have an account? "
+              : "Already have an account? "}
+          </p>
+          <Link
+            href={type == "sign-in" ? "/sign-up" : "/sign-in"}
+            className="text-brand ml-1 font-medium"
+          >
+            {type == "sign-in" ? "Sign Up" : "Sign In"}
+          </Link>
+        </div>
       </form>
     </>
   );
