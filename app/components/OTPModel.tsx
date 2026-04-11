1adab3c6b6a09e86757d8db15d3d69f4d9a67d4d
franckrm <franckrm2001@gmail.com>
2026-04-11 13:25:39 -0300

    wip: add otp code validation frontend

--- app/components/OTPModel.tsx
+++ app/components/OTPModel.tsx
@@ -12,6 +12,13 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
 function OTPModel() {
   return (
     <AlertDialog>
@@ -26,6 +33,19 @@ function OTPModel() {
             account from our servers.
           </AlertDialogDescription>
         </AlertDialogHeader>
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
           <AlertDialogAction>Continue</AlertDialogAction>
