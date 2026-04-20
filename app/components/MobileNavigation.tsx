8392d068835c77615d1d6098002d69f652d7d2a4
franckrm <franckrm2001@gmail.com>
2026-04-20 13:56:01 -0300

    wip: mobile navigation logou button

--- app/components/MobileNavigation.tsx
+++ app/components/MobileNavigation.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import {
   Sheet,
   SheetClose,
@@ -9,7 +11,10 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 
@@ -50,23 +55,66 @@ const MobileNavigation = ({
           />
         </SheetTrigger>
         <SheetContent className="shad-sheet h-screen px-3">
-          <SheetHeader>
-            <SheetTitle>
-              <div className="header-user">
-                <Image
-                  src={avatar}
-                  alt="Avatar"
-                  width={44}
-                  height={44}
-                  className="header-user-avatar"
-                />
-                <div className="sm:hidden lg:block">
-                  <p className="subtitle-2 capitalize">{fullName} </p>
-                </div>
+          <SheetTitle>
+            <div className="header-user">
+              <Image
+                src={avatar}
+                alt="Avatar"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName} </p>
+                <p className="caption">{email}</p>
               </div>
-            </SheetTitle>
-            <SheetDescription>This action cannot be undone.</SheetDescription>
-          </SheetHeader>
+            </div>
+            <Separator className="bg-light-200/20 mb-4" />
+          </SheetTitle>
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              {navItems.map(({ url, name, icon }) => (
+                <Link href={url} key={name} className="lg:w-full">
+                  <li
+                    className={cn(
+                      "mobile-nav-item",
+                      pathname === url && "shad-active",
+                    )}
+                  >
+                    <Image
+                      src={icon}
+                      alt={name}
+                      width={24}
+                      height={24}
+                      className={cn(
+                        "nav-icon",
+                        pathname === url && "nav-icon-active",
+                      )}
+                    />
+                    <p>{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <Separator className="bg-light-200/20 my-5" />
+          <div className="flex flex-col justify-between gap-5 pb-5">
+            FileUpload
+            <form>
+              <Button
+                type="submit"
+                className="mobile-sign-out-button"
+                onClick={() => {}}
+              >
+                <Image
+                  src="/assets/icons/logout.svg"
+                  alt="logo"
+                  width={24}
+                  height={24}
+                />
+              </Button>
+            </form>
+          </div>
         </SheetContent>
       </Sheet>
     </header>
